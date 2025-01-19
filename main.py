0cf0246463e10044cbc101263ec65861e4d86fb9
cloner174 <hamedhajipour1400@gmail.com>
2025-01-20 02:18:53 +0330

    fix yaml loading by adding SafeLoad arg!

--- main.py
+++ main.py
@@ -504,7 +504,8 @@ if __name__ == '__main__':
     p = parser.parse_args()
     if p.config is not None:
         with open(p.config, 'r') as f:
-            default_arg = yaml.load(f)
+            #default_arg = yaml.load(f)
+            default_arg = yaml.load(f, Loader=yaml.SafeLoader)
         key = vars(p).keys()
         for k in default_arg.keys():
             if k not in key:
